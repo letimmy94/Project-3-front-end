b631e569593636f75524fd6976cd2e67bf241652
Timmy Le <le.timmy94@gmail.com>
2018-03-27 10:09:13 -0400

    installed axios and react-router-dom

--- global-cup/src/components/App/App.js
+++ global-cup/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 import Container from '../Container/Container'
 import Logo from '../Logo/Logo'
 import NavBar from '../NavBar/NavBar'
