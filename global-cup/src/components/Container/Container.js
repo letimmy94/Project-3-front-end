c06a571d77110a50a418c9653d7845ebe9055cee
Timmy Le <le.timmy94@gmail.com>
2018-03-28 11:29:35 -0400

    connection to database

--- global-cup/src/components/Container/Container.js
+++ global-cup/src/components/Container/Container.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 import './Container.css'
+import axios from 'axios'
 
 class Container extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      teams: []
+    }
+  }
+
+  componentDidMount() {
+    let teams = []
+    axios
+      .get('http://localhost:4000/teams')
+      .then(res => {
+        res.data.map(team => {
+          teams.push(team)
+        })
+      })
+      .then(() => {
+        this.setState({
+          teams: teams
+        })
+        console.log(this.state)
+      })
+  }
+
   render() {
     return <div> Teams will go here </div>
   }
