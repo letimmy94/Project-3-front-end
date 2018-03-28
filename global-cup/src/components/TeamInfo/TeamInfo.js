9f5a136eb1a7f5962b6604e0d8b789d0ae2d2aca
Jonathan Fragale <jtfragale@gmail.com>
2018-03-28 09:19:30 -0400

    updated

--- global-cup/src/components/TeamInfo/TeamInfo.js
+++ global-cup/src/components/TeamInfo/TeamInfo.js
@@ -16,78 +16,83 @@ class TeamInfo extends Component {
         <h4>coach:</h4>
         <h4>color here</h4>
         <table>
-          <tr>
-            <th className="firstc" />
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Position</th>
-          </tr>
-          <tr>
-            <th>Player 1</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 2</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 3</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 4</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 5</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 6</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 7</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 8</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 9</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 10</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-          <tr>
-            <th>Player 11</th>
-            <th />
-            <th />
-            <th />
-          </tr>
+          <thead>
+            <tr>
+              <th className="firstc" />
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Position</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th>Player 1</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 2</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 3</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 4</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 5</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 6</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 7</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 8</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 9</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 10</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+            <tr>
+              <th>Player 11</th>
+              <th />
+              <th />
+              <th />
+            </tr>
+          </tbody>
         </table>
       </div>
     )
