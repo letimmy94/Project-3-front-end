77e40b60c04fc5861a89cdd461cbd2c27696bedf
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-03-30 10:30:13 -0400

    edit fix

--- global-cup/src/components/EditTeam/EditTeam.js
+++ global-cup/src/components/EditTeam/EditTeam.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 
 class EditTeam extends Component {
   render() {
-    console.log(this.props)
     let team = this.props.teams.filter(team => {
       return team._id === this.props.match.params.id
     })
